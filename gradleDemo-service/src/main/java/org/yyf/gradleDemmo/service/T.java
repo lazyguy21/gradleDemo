1100b7f1e0a3d249ea573b365822700974a9f703
lazyguy21 <yeyinfei@qq.com>
2016-05-20 10:14:04 +0800

    init

---
+++ gradleDemo-service/src/main/java/org/yyf/gradleDemmo/service/T.java
@@ -0,0 +1,7 @@
+package org.yyf.gradleDemmo.service;
+
+/**
+ * Created by lazyguy on 2016-5-20.
+ */
+public class T {
+}
---
+++ gradleDemo-web/src/main/java/org/yyf/gradleDemo/web/T.java
@@ -0,0 +1,7 @@
+package org.yyf.gradleDemo.web;
+
+/**
+ * Created by lazyguy on 2016-5-20.
+ */
+public class T {
+}
