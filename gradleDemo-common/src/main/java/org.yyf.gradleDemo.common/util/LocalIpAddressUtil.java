34707b3f0a72c04c9e9c8948413f58c5cd88a657
lazyguy21 <15882041972@sina.cn>
2016-07-20 09:07:12 +0800

    IPUtil

---
+++ gradleDemo-common/src/main/java/org.yyf.gradleDemo.common/util/LocalIpAddressUtil.java
@@ -0,0 +1,103 @@
+package org.yyf.gradleDemo.common.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LocalIpAddressUtil {
+    public static void main(String[] args) throws SocketException {
+    }
+
+    /**
+     * 获取本地ip地址，有可能会有多个地址, 若有多个网卡则会搜集多个网卡的ip地址
+     */
+    public static Set<InetAddress> resolveLocalAddresses() {
+        Set<InetAddress> addrs = new HashSet<>();
+        Enumeration<NetworkInterface> ns = null;
+        try {
+            ns = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException e) {
+            // ignored...
+        }
+        while (ns != null && ns.hasMoreElements()) {
+            NetworkInterface n = ns.nextElement();
+            Enumeration<InetAddress> is = n.getInetAddresses();
+            while (is.hasMoreElements()) {
+                InetAddress i = is.nextElement();
+                if (!i.isLoopbackAddress() && !i.isLinkLocalAddress() && !i.isMulticastAddress() && !isSpecialIp(i.getHostAddress()))
+                    addrs.add(i);
+            }
+        }
+        return addrs;
+    }
+    public static String getIP(){
+        Set<String> ips = resolveLocalIps();
+        for (String ip : ips) {
+            return ip;
+        }
+        return null;
+    }
+
+    public static Set<String> resolveLocalIps() {
+        Set<InetAddress> addrs = resolveLocalAddresses();
+        Set<String> ret = new HashSet<>();
+        for (InetAddress addr : addrs)
+            ret.add(addr.getHostAddress());
+        return ret;
+    }
+
+    private static boolean isSpecialIp(String ip) {
+        if (ip.contains(":")) return true;
+        if (ip.startsWith("127.")) return true;
+        if (ip.startsWith("169.254.")) return true;
+        if (ip.equals("255.255.255.255")) return true;
+        return false;
+    }
+
+    /**
+     * 多IP处理，可以得到最终ip
+     * @return
+     */
+    public static String getIp() {
+        String localip = null;// 本地IP，如果没有配置外网IP则返回它
+        String netip = null;// 外网IP
+        try {
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            InetAddress ip = null;
+            boolean finded = false;// 是否找到外网IP
+            while (netInterfaces.hasMoreElements() && !finded) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> address = ni.getInetAddresses();
+                while (address.hasMoreElements()) {
+                    ip = address.nextElement();
+//                  System.out.println(ni.getName() + ";" + ip.getHostAddress()
+//                          + ";ip.isSiteLocalAddress()="
+//                          + ip.isSiteLocalAddress()
+//                          + ";ip.isLoopbackAddress()="
+//                          + ip.isLoopbackAddress());
+                    if (!ip.isSiteLocalAddress() && !ip.isLoopbackAddress()
+                            && ip.getHostAddress().indexOf(":") == -1) {// 外网IP
+                        netip = ip.getHostAddress();
+                        finded = true;
+                        break;
+                    } else if (ip.isSiteLocalAddress()
+                            && !ip.isLoopbackAddress()
+                            && ip.getHostAddress().indexOf(":") == -1) {// 内网IP
+                        localip = ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        if (netip != null && !"".equals(netip)) {
+            return netip;
+        } else {
+            return localip;
+        }
+    }
+}
