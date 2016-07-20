632aefc1aad3e5ce7fcf3c67d1a12447e6331164
lazyguy21 <15882041972@sina.cn>
2016-07-20 09:30:00 +0800

    全局唯一码

---
+++ gradleDemo-common/src/main/java/org.yyf.gradleDemo.common/util/UniqRequestIdGen.java
@@ -0,0 +1,31 @@
+package org.yyf.gradleDemo.common.util;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class UniqRequestIdGen {
+
+    private static AtomicLong lastId         = new AtomicLong();                                         // 自增id，用于requestId的生成过程
+    private static final long   startTimeStamp = System.currentTimeMillis();                               // 启动加载时的时间戳，用于requestId的生成过程
+    private static final String ip             = LocalIpAddressUtil.getIp(); // 本机ip地址，用于requestId的生成过程
+
+    public static void main(String[] args) {
+        System.out.println(resolveReqId());
+    }
+
+    private static String resolveReqId() {
+        // 规则： hexIp(ip)base36(timestamp)-seq
+        return hexIp(ip) + Long.toString(startTimeStamp, Character.MAX_RADIX) + "-" + lastId.incrementAndGet();
+    }
+
+    // 将ip转换为定长8个字符的16进制表示形式：255.255.255.255 -> FFFFFFFF
+    private static String hexIp(String ip) {
+        StringBuilder sb = new StringBuilder();
+        for (String seg : ip.split("\\.")) {
+            String h = Integer.toHexString(Integer.parseInt(seg));
+            if (h.length() == 1) sb.append("0");
+            sb.append(h);
+        }
+        return sb.toString();
+    }
+
+}
